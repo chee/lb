3f00f01de95cbbd0d15bee0d9a098a3425ce8c09
chee <yay@chee.party>
2025-07-21 01:42:43 +0100

    she's a femme fatale

--- littlebook/build/build.js
+++ littlebook/build/build.js
@@ -1,4 +1,8 @@
 import options, {machineOptions} from "./options.js"
 import * as esbuild from "esbuild"
+import {VFSPackager} from "./vfs.js"
 esbuild.build(options)
 esbuild.build(machineOptions)
+const vfs = new VFSPackager()
+const system = new URL(import.meta.resolve("../system")).pathname
+vfs.packDirectory(system, `${options.outdir}/system.json`)
--- littlebootstraps/tauri/littlebootstraps.js
+++
@@ -1,4 +0,0 @@
-import esbuild from "./esbuild.js"
-const read = async path => {
-	return window.__TAURI__.fs.read
-}
